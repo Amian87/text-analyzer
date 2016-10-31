eb7d6f30d6485eb4d7abdc6d11e7ecba8196a2af
Abdulla Mian <miana@MianAMac01.clayco.net>
2016-10-30 21:37:46 -0500

    unique word count wip

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
+ 
  $(document).ready(function(){
-
  	$('button').click(function(){
- 		$('text-report').removeClass('.hidden');
+ 		$('').addClass('.hidden');
  		
  		var userText = $('#user-text').val();
 
  		// Total word count
-		var wordCount = userText.split(" ").length;
-		console.log(wordCount);
+		var wordCount = userText.split(/\s/).length;;
+		$('.word-count').text(wordCount);
+		console.log('hey ' + wordCount);
+
+		
+		// Unique word count
+		var textArray = userText.split(" ");
+		var compareArray = userText.split(" ");
+
+			var uniqueWordCount = 1;
+		    
+		    if(textArray.indexOf(compareArray) === -1){
+        		console.log(uniqueWordCount++);
+       		}
+
+		
 
 		//Average word length in characters
 		var totalCharacters = userText.replace(/\s+/g, '').split('').length;
 		var averageWordLength = totalCharacters / wordCount;
-		console.log(averageWordLength);
-
-		// Unique word count
-
-		// var textArray = userText.split('');
-
-		// var uniqueWords = 1;
-		// for (var i = 0; i < textArray.length; i++){
-		//     if(textArray[i] == " ") {
-		//         console.log(uniqueWords++);
-		//     }    
-		// }
-
-
-
+		$('.average-word').text(averageWordLength);
+		
+		//Average sentence length
 		var numberOfSenteces = userText.split('\n').length;
 		// console.log(numberOfSenteces); 
 
@@ -42,10 +44,5 @@
 
 		//         }
 		           
-		
-
- 		
-
-
  	});
  });
