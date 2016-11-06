6e25f6e2121f47eaf8c01be08ecce995cc78a356
Abdulla Mian <miana@MianAMac01.clayco.net>
2016-11-06 13:29:37 -0600

    wip average sentence length

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
- 
- $(document).ready(function(){
+  $(document).ready(function(){
  	
  	$('button').click(function(){
  		
@@ -8,45 +7,85 @@
  		var userText = $('#user-text').val();
 
  		// Total word count 
-		var wordCount = userText.split(/\s/).length;;
+		var wordCount = userText.split(/\s/).length;
+		// console.log(wordCount);
 		$('.word-count').text(wordCount);
-		console.log('hey ' + wordCount);
-
 		
 		// Unique word count
 		var textArray = userText.split(" ");
 		var compareArray = userText.split(" ");
+		// console.log(textArray);
 
-			var uniqueWordCount = 1;
-		    
-		    if(textArray.indexOf(compareArray) === -1){
-        		console.log(uniqueWordCount++);
-       		}
 
-		
+
+
 
 		//Average word length in characters
 		var totalCharacters = userText.replace(/\s+/g, '').split('').length;
-		var averageWordLength = totalCharacters / wordCount;
+		var averageWordLength = (totalCharacters / wordCount);
 		$('.average-word').text(averageWordLength);
 		
 		
 
 		//Average sentence length
-		var numberOfSenteces = userText.split('\n').length;
-		// console.log(numberOfSenteces); 
+		var numberOfSentences = userText.split(/\.\s/);
+		console.log(numberOfSentences);
+
+		for (var i = 0; i < numberOfSentences.length; i++) {
+			
+			var lengths = []
+			lengths.push(numberOfSentences[i].length);
+			console.log(lengths);
+
+		}
 
 
-		//     var textArray = userText.split(" ");
-		//     var uniqueWord = 1;
-		//     for (var i = 0 ; i < textArray.length; i++){
-		//            if (textArray[i] !== textArray[i]) {
-		//             console.log(uniqueWord++);
+		    // var textArray = userText.split(" ");
+		    // var uniqueWord = 1;
+		    // for (var i = 0 ; i < textArray.length; i++){
+		    //        if (textArray[i] !== textArray[i]) {
+		    //         console.log(uniqueWord++);
 		                
 		            
-		//          }
+		    //      }
 
-		//         }
+		    //     }
 		           
  	});
  });
+
+
+
+
+
+  	// var uniqueWordCount = 1;
+
+
+		//     for (var i = 0 ; i < textArray.length; i++){
+		//            if (textArray.indexOf(compareArray) != -1) {
+		//             console.log(uniqueWordCount++);
+		  		            
+		//          }
+
+		//         }
+		    
+		//     if(textArray.indexOf(compareArray) != -1){
+  //       		console.log('hey');
+  //      		}
+
+
+  // 		var counts = {};
+		// for (var i = 0; i < textArray.length; i++) {
+  //   	counts[textArray[i]] = 1 + (counts[textArray[i]] || 0);
+		// }
+		
+  // 		var results = [];
+
+ 	// 	for(var i=0; i<counts.length; i++) {
+  // 		for(key in counts[i]) {
+  //   	if(counts[i][key].indexOf(counts)!=-1) {
+  //     	results.push(counts[i]);
+  //     	console.log(results);
+		//     }
+		//   }
+		// }
