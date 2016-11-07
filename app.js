0c24098e6827c964d8b1d11420ca8aa67483828f
Abdulla Mian <woodworks87@gmail.com>
2016-11-07 07:50:35 -0600

    solution wrapped in function

--- app.js
+++ app.js
@@ -6,15 +6,16 @@
 
  		var userText = $('#user-text').val();
 
+ 		anlayzeText(userText);
+
+ 		function anlayzeText(userText){
+
  		// Total word count 
 		var wordCount = userText.split(/\s/).length;
-		// console.log(wordCount);
 		$('.word-count').text(wordCount);
 		
 		// Unique word count
 		var textArray = userText.split(" ");
-		var compareArray = userText.split(" ");
-
 
 		var results = []
 		var counter = 0
@@ -22,51 +23,25 @@
 		for (var i = 0; i < textArray.length; i++){
 			if (results.indexOf(textArray[i]) === -1 ){
 				results.push(textArray[i]);
-				console.log(results);
 			}
 
 		}
 
-
-
+		var uniqueWordCount = results.length
+		$('.unique-words').text(uniqueWordCount);
 
 		//Average word length in characters
 		var totalCharacters = userText.replace(/\s+/g, '').split('').length;
 		var averageWordLength = (totalCharacters / wordCount);
 		$('.average-word').text(averageWordLength);
 		
-		
 
 		// Average sentence length
 		var numberOfSentences = userText.split(/\.\s/).length;
 		var averageSentenceLength = totalCharacters / numberOfSentences;
+		$('.average-sentence').text(averageSentenceLength);
 
-		console.log(averageSentenceLength);
-
-		// for (var i = 0; i < numberOfSentences.length; i++) {
-		// 	var lengths = []
-		// 	lengths.push(numberOfSentences[i].length);
-		// 	console.log(lengths);
-
-		// }
-
-
-		    // var textArray = userText.split(" ");
-		    // var uniqueWord = 1;
-		    // for (var i = 0 ; i < textArray.length; i++){
-		    //        if (textArray[i] !== textArray[i]) {
-		    //         console.log(uniqueWord++);
-		                
-		            
-		    //      }
-
-		    //     }
-		           
+	  }
  	});
  });
 
-
-
-
-
-  	
